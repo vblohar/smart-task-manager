765f6b7dde2341b387a5909670f20d6db467ff94
Vaibhav L <loharvb@gmail.com>
2026-02-21 12:09:59 +0530

    Added packages and routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+            import('./features/tasks/pages/task-list/task-list')
+                .then(m => m.TaskList)
+    },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
