e5acc71da8236b329a19942ff087bec07eaf744e
Vaibhav Lohar <loharvb@gmail.com>
2026-02-21 13:52:47 +0530

    refactor routing structure to include main layout component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,27 @@
 import { Routes } from '@angular/router';
 
+
 export const routes: Routes = [
     {
         path: '',
         loadComponent: () =>
-            import('./features/tasks/pages/task-list/task-list')
-                .then(m => m.TaskList)
-    },
+            import('./core/layout/main-layout/main-layout')
+                .then(m => m.MainLayout),
+  
+        children: [
+            {
+                path: '',
+                loadComponent: () =>
+                    import('./features/tasks/pages/task-list/task-list')
+                        .then(m => m.TaskList)
+            },
+            {
+                path: 'create',
+                loadComponent: () =>
+                    import('./features/tasks/pages/task-form/task-form')
+                        .then(m => m.TaskForm)
+            }
+        ]
+    }
+
 ];
